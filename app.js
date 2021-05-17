3e39ff65cd93d575787355a46f32ddb4f96415d9
Daniel Fischer <dev_dafi@icloud.com>
2021-05-17 12:08:12 +0200

    Change authentication to Sign In with google. Add styles for login button.

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const findOrCreate = require('mongoose-findorcreate');
 
 // PASSPORT & SESSION RELATED REQUIREMENTS
 
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+// GOOGLE OAUTH RELATED REQUIREMENTS
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
 
 const app = express();
 const port = 3000;
@@ -52,9 +60,11 @@ app.use(passport.session());
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // MODEL
 
@@ -64,14 +74,53 @@ const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// GOOGLE AOUTH SETUP
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  // console.log(profile);
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 // GET ROUTES
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+// Use passport to authenticate user with google strategy. Scope contains email and userID
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+
 
 app.get("/login", (req, res) => {
   res.render("login");
@@ -87,7 +136,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-  console.log(req.isAuthenticated());
+  // console.log(req.isAuthenticated());
   if (req.isAuthenticated()) {
     res.render("secrets");
   } else {
